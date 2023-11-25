614ce6d18e85583d0ac29790f83082933d9a1291
arsuhinars <arsuhinars@gmail.com>
2023-11-25 09:12:16 +0400

    Added user preloading

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
-  <RouterView />
+  <RouterView v-if="!isLoading" />
 </template>
 
 <style></style>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useAuthStore } from './stores/auth'
+import { pushErrorPage } from './utils'
+
+const auth = useAuthStore()
+
+const isLoading = ref<boolean>(false)
+
+onMounted(async () => {
+  isLoading.value = true
+
+  try {
+    await auth.updateUser()
+  } catch (error) {
+    pushErrorPage(error)
+  } finally {
+    isLoading.value = false
+  }
+})
+</script>
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,30 +1,31 @@
 import { defineStore } from 'pinia'
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { type User } from '@/schemas/users'
 import router from '@/router'
 import { getCurrentUser } from '@/api/users'
 import { ApiError } from '@/api/utils'
 import { pushErrorPage } from '@/utils'
 
+const AUTH_CREDENTIALS_KEY = 'authCredentials'
+
 export const useAuthStore = defineStore('auth', () => {
-  const _credentials = ref<string | null>(null)
-  const _password = ref<string | null>(null)
+  const _credentials = ref<string | null>(localStorage.getItem(AUTH_CREDENTIALS_KEY))
   const _user = ref<User | null>(null)
 
   const credentials = computed(() => _credentials.value)
   const user = computed(() => _user.value)
-  const password = computed(() => _password.value)
+  const password = computed(() =>
+    _credentials.value === null ? null : atob(_credentials.value).split(':')[1]
+  )
   const isAuthorized = computed(() => _user.value !== null)
 
   async function tryLogin(login: string, password: string): Promise<boolean> {
     try {
       _credentials.value = btoa(`${login}:${password}`)
-      _password.value = password
       _user.value = await getCurrentUser()
       return true
     } catch (error) {
       _credentials.value = null
-      _password.value = null
       _user.value = null
       if (error instanceof ApiError && error.statusCode == 401) {
         return false
@@ -35,22 +36,33 @@ export const useAuthStore = defineStore('auth', () => {
 
   async function logout() {
     _credentials.value = null
-    _password.value = null
     _user.value = null
     router.push({ name: 'login' })
   }
 
   async function updateUser() {
-    if (!isAuthorized.value) {
+    if (_credentials.value === null) {
       return
     }
 
     try {
       _user.value = await getCurrentUser()
     } catch (error) {
-      pushErrorPage(error)
+      if (error instanceof ApiError && error.statusCode == 401) {
+        _credentials.value = null
+      } else {
+        pushErrorPage(error)
+      }
     }
   }
+
+  watch(_credentials, () => {
+    if (_credentials.value === null) {
+      localStorage.removeItem(AUTH_CREDENTIALS_KEY)
+    } else {
+      localStorage.setItem(AUTH_CREDENTIALS_KEY, _credentials.value)
+    }
+  })
 
   return { credentials, password, user, isAuthorized, tryLogin, logout, updateUser }
 })
