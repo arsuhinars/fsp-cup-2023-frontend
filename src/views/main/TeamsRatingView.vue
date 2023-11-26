3ad919b688da99b7ac839a9d3bf1628b6afcd82e
arsuhinars <arsuhinars@gmail.com>
2023-11-26 14:44:37 +0400

    Added test dataset in TeamsRatingView

--- src/views/main/TeamsRatingView.vue
+++ src/views/main/TeamsRatingView.vue
@@ -37,5 +37,54 @@ const tableColumns: TableColumn[] = [
   }
 ]
 
-const tableItems: TeamAssessment[] = []
+const tableItems: TeamAssessment[] = [
+  {
+    team_name: 'Infinity',
+    team_discipline: 'Dota 2',
+    team_av_score: 31.018436,
+    team_deviation: 1.739091
+  },
+  {
+    team_name: 'Sprout',
+    team_discipline: 'CS 2',
+    team_av_score: 30.061712,
+    team_deviation: 0.301454
+  },
+  {
+    team_name: 'BIG',
+    team_discipline: 'CS 2',
+    team_av_score: 28.471558,
+    team_deviation: 0.232612
+  },
+  {
+    team_name: 'Apeks',
+    team_discipline: 'CS 2',
+    team_av_score: 19.762486,
+    team_deviation: -0.144428
+  },
+  {
+    team_name: 'ALTERNATE',
+    team_discipline: 'CS 2',
+    team_av_score: 14.098473,
+    team_deviation: -0.389638
+  },
+  {
+    team_name: 'BOOM Esports',
+    team_discipline: 'Dota 2',
+    team_av_score: 6.86494,
+    team_deviation: -0.39379
+  },
+  {
+    team_name: 'Mad Kings',
+    team_discipline: 'Dota 2',
+    team_av_score: 5.925756,
+    team_deviation: -0.476725
+  },
+  {
+    team_name: 'BocaJuniors',
+    team_discipline: 'Dota 2',
+    team_av_score: 1.48829,
+    team_deviation: -0.868576
+  }
+]
 </script>
